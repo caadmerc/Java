d2b4732940d8d6efd7a757729dd7f423324ca45d
CarlosMerchanAnchundia <CarlosMerchanAnchundia@192.168.1.113>
2018-07-26 12:47:42 -0500

    Modificacion de codigo

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,7 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World test");
     }
     
 }
