3e7c4b9774020e0c70277eb37b60c000f1e0a6d5
CarlosMerchanAnchundia <CarlosMerchanAnchundia@192.168.1.113>
2018-07-26 12:53:42 -0500

    Borrado de Comentarios por defecto

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -11,9 +11,6 @@ package javaapplication1;
  */
 public class JavaApplication1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello World test");
     }
