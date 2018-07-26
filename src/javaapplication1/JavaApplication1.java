147099d15993aae60d6835ecc341941733b3f404
CarlosMerchanAnchundia <CarlosMerchanAnchundia@192.168.1.113>
2018-07-26 13:02:32 -0500

    Se Elimino el Header, y se dio formato al codigo

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication1;
 
 /**
  *
  * @author Carlos Merchan Anchundia
  */
+
 public class JavaApplication1 {
 
     public static void main(String[] args) {
