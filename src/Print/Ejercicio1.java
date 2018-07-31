e1b802c4ef3a5cf60c340e1f41e0657f3b7bb7a7
CarlosMerchanAnchundia <CarlosMerchanAnchundia@192.168.1.113>
2018-07-31 00:04:18 -0500

    Se modifico los nombres de: Clase, Paquete y archivo Creacion de Programa que muestra por pantalla 3 textos

---
+++ src/Print/Ejercicio1.java
@@ -0,0 +1,23 @@
+package Print;
+
+// Programa que imrpime mensajes 
+
+/**
+ *
+ * @author Carlos Merchan Anchundia
+ */
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("This\nIs My\nFirst \"Java Program\"");
+        System.out.print("\nThis is the second Statement\n");
+        System.out.printf("%nThis is my last statement\n");        
+    }// End Method Main   
+}// End Class Ejercicio1
+
+
+/*
+Print puede recibir
+Boolean, char, int, double, float, long, String, Object
+*/
