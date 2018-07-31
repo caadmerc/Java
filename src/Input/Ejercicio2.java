c1b252cdedb62fa920dd7d375d7918272562c51a
CarlosMerchanAnchundia <CarlosMerchanAnchundia@192.168.1.113>
2018-07-31 00:37:23 -0500

    Formato del Texto en la Clase Ejercicio2 del Paquete Input

--- src/Input/Ejercicio2.java
+++ src/Input/Ejercicio2.java
@@ -13,18 +13,18 @@ public class Ejercicio2 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int n1,// Numero 1
-            n2;// Numero 1
+            n2;// Numero 2
         
         System.out.print("Ingrese el Número 1: ");
         n1=input.nextInt();
-        System.out.print("Ingrese el numero 2: ");
+        System.out.print("Ingrese el Número 2: ");
         n2=input.nextInt();
         
-        System.out.printf("La suma de los numeros %d y %d es: %d%n",n1,n2,(n1+n2));
-        System.out.printf("La resta entre %d y %d es: %d%n",n1,n2,(n1-n2));
-        System.out.printf("La multiplicacion es: %d\n",n1*n2);
-        System.out.printf("La division es: %d\n",n1/n2);
-        System.out.printf("El Modulo es: %d\n",n1%n2);
+        System.out.printf("La Suma de los Números %d y %d es: %d%n",n1,n2,(n1+n2));
+        System.out.printf("La Resta entre %d y %d es: %d%n",n1,n2,(n1-n2));
+        System.out.printf("La Multiplicación es: %d\n",n1*n2);
+        System.out.printf("La División es: %d\n",n1/n2);
+        System.out.printf("El Módulo es: %d\n",n1%n2);
         
     }// End Method Main
 }//End Class Ejercicio 2
