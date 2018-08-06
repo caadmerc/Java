369c3f47508d6bc7fa844f7c110b0e2cb026ddd7
CarlosMerchanAnchundia <CarlosMerchanAnchundia@192.168.1.113>
2018-08-06 18:57:40 -0500

    Modificacion del constructor Account Se creo el Metodo deposit y getBalance

--- src/BankConstructor/Account.java
+++ src/BankConstructor/Account.java
@@ -5,14 +5,32 @@ package BankConstructor;
  * @author Carlos Merchan Anchundia
  */
 public class Account {
-
     private String name;// variable de instancia 
-
+    private double balance;// variable de instancia
+    
     // Creacion del constructor
-    public Account(String name) {
+    public Account(String name, double balance) {
         this.name = name;
+        
+        //valida si el balance es positivo y mayor a 0.0
+        if (balance > 0.0) {
+            this.balance=balance;
+        }
     }
-
+    
+    //Metodo que permite depositar dinero 
+    public void deposit(double deposit){
+        //Valida si la cantidad ingresada es Positiva y mayor a 0.0
+        if (deposit > 0.0) {
+            balance+=deposit;
+        }
+    }
+    
+    //Metodo que retorna el balance de la cuenta
+    public double getBalance(){
+        return balance;
+    }
+    
     //Metodo getter    
     public String getName() {
         return name;
